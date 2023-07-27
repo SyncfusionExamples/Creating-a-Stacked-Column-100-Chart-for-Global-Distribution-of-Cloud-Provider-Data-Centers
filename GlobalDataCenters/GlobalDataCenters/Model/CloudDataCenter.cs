01c07e15b1f3047e6765dfe842eb9ff92f0a48fa
SaiyathAliFathima <103025761+SaiyathAliFathima@users.noreply.github.com>
2023-07-27 19:03:41 +0530

    updated

--- GlobalDataCenters/GlobalDataCenters/Model/CloudDataCenter.cs
+++ GlobalDataCenters/GlobalDataCenters/Model/CloudDataCenter.cs
@@ -10,15 +10,16 @@ namespace GlobalDataCenters
     public class CloudDataCenter
     {
         public string Cloud { get; set; }
-        public double[] SoutheastAsia { get; set; }
-        public double[] NorthAmerica { get; set; }
-        public double[] Europe { get; set; }
-        public double[] Others { get; set; }
+        public double SoutheastAsia { get; set; }
+        public double NorthAmerica { get; set; }
+        public double Europe { get; set; }
+        public double Others { get; set; }
 
-        //For Data Labels
-        public string SoutheastAsiaValue => $"{SoutheastAsia[0]} ({SoutheastAsia[1]}%)";
-        public string NorthAmericaValue => $"{NorthAmerica[0]} ({NorthAmerica[1]}%)";
-        public string EuropeValue => $"{Europe[0]} ({Europe[1]}%)";
-        public string OthersValue => $"{Others[0]} ({Others[1]}%)";
+        // For Data Labels
+        private double Total => SoutheastAsia + NorthAmerica + Europe + Others;
+        public string SoutheastAsiaValue => $"{SoutheastAsia} ({((SoutheastAsia / Total) * 100):F0}%)";
+        public string NorthAmericaValue => $"{NorthAmerica} ({((NorthAmerica / Total) * 100):F0}%)";
+        public string EuropeValue => $"{Europe} ({((Europe / Total) * 100):F0}%)";
+        public string OthersValue => $"{Others} ({((Others / Total) * 100):F0}%)";
     }
 }
